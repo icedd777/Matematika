6d295a2691f2e1fc1c677eb9598bf170d375a2b8
icedd777 <dumatubunrisat@gmail.com>
2024-11-24 17:48:53 +0800

    Update MatematikaCanggihBeraksi.java

--- MatematikaCanggihBeraksi.java
+++ MatematikaCanggihBeraksi.java
@@ -5,18 +5,18 @@
 
 /**
  *
- * @author angga
+ * @author Risat
  */
 public class MatematikaCanggihBeraksi {
     public static void main(String[] args) {
         MatematikaCanggih mtkCanggih = new MatematikaCanggih();
 
-        System.out.println("Tambah: " + mtkCanggih.tambah(30, 20));
-        System.out.println("Kurang: " + mtkCanggih.kurang(65, 25));
-        System.out.println("Kali: " + mtkCanggih.kali(78, 31));
-        System.out.println("Bagi: " + mtkCanggih.bagi(120, 20));
-        System.out.println("Modulus: " + mtkCanggih.modulus(56, 3));
-        System.out.println("Kombinasi (Tambah + Kali): " + mtkCanggih.kombinasiTambahKali(55, 35, 300));
+        System.out.println("Tambah: " + mtkCanggih.tambah(20, 20));
+        System.out.println("Kurang: " + mtkCanggih.kurang(75, 35));
+        System.out.println("Kali: " + mtkCanggih.kali(98, 21));
+        System.out.println("Bagi: " + mtkCanggih.bagi(172, 60));
+        System.out.println("Modulus: " + mtkCanggih.modulus(57, 4));
+        System.out.println("Kombinasi (Tambah + Kali): " + mtkCanggih.kombinasiTambahKali(53, 25, 200));
     }
 }
 
